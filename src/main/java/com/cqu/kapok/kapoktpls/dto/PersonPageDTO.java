288f72beffd872facb31f2f76957bd2d153de37b
卓钊 <18350120264@163.com>
2022-06-28 17:02:13 +0800

    bug

---
+++ src/main/java/com/cqu/kapok/kapoktpls/dto/PersonPageDTO.java
@@ -0,0 +1,21 @@
+package com.cqu.kapok.kapoktpls.dto;
+
+import com.cqu.kapok.kapoktpls.entity.Person;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @
+ * @Description
+ * @Author 卓钊
+ * @Date 2022/6/28 16:47
+ * @Version 1.0
+ **/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PersonPageDTO extends Person {
+    int size;
+    int page;
+}
