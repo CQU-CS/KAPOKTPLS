458e3451812cf7f41874676a37bb0da0f8d19f3e
卓钊 <18350120264@163.com>
2022-07-01 22:19:19 +0800

    首页所需半年总收入和总支出

--- src/main/java/com/cqu/kapok/kapoktpls/controller/StatisticController.java
+++ src/main/java/com/cqu/kapok/kapoktpls/controller/StatisticController.java
@@ -9,9 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @
@@ -140,6 +138,157 @@ public class StatisticController {
         }finally {
             return buildingSalePrice;
         }
+    }
+
+    /**
+     * 半年收入
+     * @param dateString
+     * @return
+     * @throws ParseException
+     */
+    @PostMapping("getHalfYearIncome")
+    public DataResult getHalfYearIncome(String dateString) throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = format.parse(dateString);
+        //获取前半年日期
+        Date date1 = stepMonth(date,-1);
+        Date date2 = stepMonth(date,-2);
+        Date date3 = stepMonth(date,-3);
+        Date date4 = stepMonth(date,-4);
+        Date date5 = stepMonth(date,-5);
+        //根据日期获取值
+        long totalIncome = getTotalIncome(date);
+        long totalIncome1 = getTotalIncome(date1);
+        long totalIncome2 = getTotalIncome(date2);
+        long totalIncome3 = getTotalIncome(date3);
+        long totalIncome4 = getTotalIncome(date4);
+        long totalIncome5 = getTotalIncome(date5);
+        //装入列表
+        List<Long> longList = new ArrayList<>();
+        longList.add(totalIncome);
+        longList.add(totalIncome1);
+        longList.add(totalIncome2);
+        longList.add(totalIncome3);
+        longList.add(totalIncome4);
+        longList.add(totalIncome5);
+        Map<String,Object> map = new HashMap<>();
+        map.put("totalIncomes",longList);
+        //返回数值
+        return DataResult.successByData(map);
+    }
+
+    /**
+     * 半年总支出
+     * @param dateString
+     * @return
+     * @throws ParseException
+     */
+    @PostMapping("getHalfYearExpenditures")
+    public DataResult getHalfYearExpenditures(String dateString) throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = format.parse(dateString);
+        //获取前半年日期
+        Date date1 = stepMonth(date,-1);
+        Date date2 = stepMonth(date,-2);
+        Date date3 = stepMonth(date,-3);
+        Date date4 = stepMonth(date,-4);
+        Date date5 = stepMonth(date,-5);
+        //根据日期获取值
+        long totalExpenditures = getTotalExpenditures(date);
+        long totalExpenditures1 = getTotalExpenditures(date1);
+        long totalExpenditures2 = getTotalExpenditures(date2);
+        long totalExpenditures3 = getTotalExpenditures(date3);
+        long totalExpenditures4 = getTotalExpenditures(date4);
+        long totalExpenditures5 = getTotalExpenditures(date5);
+        //装入列表
+        List<Long> longList = new ArrayList<>();
+        longList.add(totalExpenditures);
+        longList.add(totalExpenditures1);
+        longList.add(totalExpenditures2);
+        longList.add(totalExpenditures3);
+        longList.add(totalExpenditures4);
+        longList.add(totalExpenditures5);
+        Map<String,Object> map = new HashMap<>();
+        map.put("totalExpenditureses",longList);
+        //返回数值
+        return DataResult.successByData(map);
+    }
+
+    /**
+     * 某月总收入
+     * @param date
+     * @return
+     */
+    public long getTotalIncome(Date date){
+        //本月建筑出售收入
+        long buildingSalePrice = 0;
+        if(this.buildingSaleService.getMonthPrice(date)!=null){
+            buildingSalePrice = this.buildingSaleService.getMonthPrice(date);
+        }
+        //本月建筑出租收入
+        long buildingRentPrice = 0;
+        if(this.buildingRentService.getMonthPrice(date)!=null){
+            buildingRentPrice = this.buildingRentService.getMonthPrice(date);
+        }
+        //本月商品出售收入
+        long goodSalePrice = 0;
+        if(this.goodsSaleService.getMonthPrice(date)!=null){
+            goodSalePrice = this.goodsSaleService.getMonthPrice(date);
+        }
+        //本月物资出售收入
+        long materialPrice = 0;
+        if(this.materialSaleService.getMonthPrice(date)!=null){
+            materialPrice = this.materialSaleService.getMonthPrice(date);
+        }
+        //本月运输收入
+        long transpositionPrice = 0;
+        if(this.transportationTaskService.getProfit(date)!=null){
+            transpositionPrice = this.transportationTaskService.getProfit(date);
+        }
+        //本月汽车出售收入
+        long truckSalePrice = 0;
+        if(this.truckSaleService.getMonthProfit(date)!=null){
+            truckSalePrice = this.truckSaleService.getMonthProfit(date);
+        }
+        //总收入
+        long totalIncome = buildingSalePrice+buildingRentPrice+goodSalePrice+materialPrice+transpositionPrice+truckSalePrice;
+        return totalIncome;
+    }
+
+    public long getTotalExpenditures(Date date){
+        //本月商品购买支出
+        long goodPurchesePrice = 0;
+        if(this.goodsPurchaseService.getMonthPrice(date)!=null){
+            goodPurchesePrice = this.goodsPurchaseService.getMonthPrice(date);
+        }
+        //本月物资购买支出
+        long materialPurchesePrice = 0;
+        if(this.materialPurchaseService.getMonthPrice(date)!=null){
+            materialPurchesePrice = this.materialPurchaseService.getMonthPrice(date);
+        }
+        //本月汽车购买支出
+        long truckPurchasePrice = 0;
+        if(this.truckPurchaseService.getMonthCost(date)!=null){
+            truckPurchasePrice = this.truckPurchaseService.getMonthCost(date);
+        }
+        //总支出
+        long totalExpenditures = goodPurchesePrice+materialPurchesePrice+truckPurchasePrice;
+        return totalExpenditures;
+    }
+
+    /**
+     *      * 在给定的日期加上或减去指定月份后的日期
+     *      *
+     *      * @param sourceDate 原始时间
+     *      * @param month      要调整的月份，向前为负数，向后为正数
+     *      * @return
+     *
+     */
+    public static Date stepMonth(Date sourceDate, int month) {
+        Calendar c = Calendar.getInstance();
+        c.setTime(sourceDate);
+        c.add(Calendar.MONTH, month);
+        return c.getTime();
     }
 
 }
